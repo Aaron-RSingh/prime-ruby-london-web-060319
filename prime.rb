3ad753d446fbdfec566723273a9a9c9f441b0a82
Aaron Ramharacksingh <aaron.ramharacksingh@gmail.com>
2019-05-29 21:50:15 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
 # Add  code here!
 def prime?(num)
   (2..Math.sqrt(num)).each { |i|
-  if num % i == 0 && i < num
+  if num < 0 
     return false
-  elsif num < 0 
-  return false
+    elsif num % i == 0 && i < num
+    return false
   end
   }
   true
